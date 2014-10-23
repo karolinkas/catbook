1aa3a545fec690e0f0619910dae62fc987d84569
Karolinka <li-nee@gmx.de>
2014-10-23 10:55:11 +0200

    added colors :-)

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,21 @@
 class ApiController < ApplicationController
 
 	def show 
-		@cat
+		cat = Cat.visible.find(params[:id])
+
+		result = {}.tap do |cat_hash|
+			cat_hash[:name]
+			cat_hash[:birthday]
+			cat_hash[:followers_count]
+			cat_hash[:followed_by_count]
+			if fields.include?("links")
+				cat_hash[:links] = {
+					public: cat_url(cat),
+					api: api_v1_cat_url(cat)
+				}
+
+
+
 	end
-	
+
 end
